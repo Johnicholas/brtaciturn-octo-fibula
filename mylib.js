6e11f61bd3346b0f8453ab9b2905a4b97d9cf176
Johnicholas Hines <johnicholas@johnicholas.com>
2015-12-14 14:40:53 -0500

    An intense weekend of work

--- mylib.js
+++ mylib.js
@@ -1,34 +1,70 @@
 'use strict';
 
 mergeInto(LibraryManager.library, {
-    printey: function() {
-        Module.print('hello from lib!');
-    },
-    new_bunny: function(which, x, y) {
-        var it = new PIXI.Sprite(PIXI.loader.resources.bunny.texture);
+    new_sprite: function(which, x, y, texture_id, layer_id) {
+        // console.log('new sprite', which, x, y, texture_id, layer_id);
+        var it = new PIXI.Sprite(textures[texture_id])
         it.anchor.x = 0.5;
         it.anchor.y = 0.5;
         it.position.x = x;
         it.position.y = y;
-        bunnies[which] = it;
-        stage.addChild(it);
+        sprites[which] = it;
+        // TERRIBLE HACK!
+        if (layer_id == 1) {
+            sprites[which].scale.x = 0.8;
+            sprites[which].scale.y = 0.8;
+        }
+        layers[layer_id].addChild(it);
     },
-    free_bunny: function(which) {
-        stage.removeChild(bunnies[which]);
-        delete bunnies[which];
+    set_sprite_position: function(which, x, y, flipped) {
+        if (sprites[which]) {
+            sprites[which].position.x = x;
+            sprites[which].position.y = y;
+            if (flipped == 1) {
+                sprites[which].scale.x = Math.abs(sprites[which].scale.x);
+            } else {
+                sprites[which].scale.x = -1 * Math.abs(sprites[which].scale.x);
+            }
+        }
     },
-    spin_bunny: function(which) {
-        bunnies[which].rotation += 0.01;
+    set_sprite_tint: function(which, tint) {
+        if (sprites[which]) {
+            sprites[which].tint = tint;
+        } else {
+            console.log('cannot set sprite tint ', which);
+        }
     },
-    set_bunny_position: function(which, x, y) {
-        bunnies[which].position.x = x;
-        bunnies[which].position.y = y;
+    set_sprite_rotation: function(which, rotation) {
+        if (sprites[which]) {
+            sprites[which].rotation = rotation;
+        } else {
+            console.log('cannot set sprite rotation ', which);
+        }
     },
-    new_splash: function(which) {
-        stage.addChild(stages[which]);
+    set_count: function(tier, new_value) {
+        console.log('set_count', tier, new_value);
+        counts[tier].text = new_value.toString();
     },
-    free_splash: function(which) {
-        stage.removeChild(stages[which]);
+    free_sprite: function(which) {
+        // console.log('free sprite', which);
+        var limit = layers.length;
+        var to_remove = sprites[which];
+        for (i = 0; i < limit; i++) {
+            layers[i].removeChild(to_remove);
+        }
+        delete sprites[which];
+    },
+    print_debug: function(message) {
+        debug_message.text = Pointer_stringify(message);
+    },
+    set_muted: function(new_muted) {
+        if (new_muted) {
+            document.querySelector('audio').muted = true;
+            sound_on_icon.visible = false;
+        } else {
+            document.querySelector('audio').muted = false;
+            sound_on_icon.visible = true;
+        }
     }
 });
 
--- myscript.js
+++ myscript.js
@@ -1,69 +1,57 @@
 'use strict'
 
-document.onkeydown = function document_onkeydown(event) {
-  event.preventDefault();
-  Module.ccall('key_down', null, ['number'], [event.keyCode]);
-}
+var renderer = new PIXI.WebGLRenderer(1280, 900);
 
-document.onkeyup = function document_onkeyup(event) {
-  event.preventDefault();
-  Module.ccall('key_up', null, ['number'], [event.keyCode]);
-}
-
-var renderer = PIXI.autoDetectRenderer(600, 400);
-document.body.appendChild(renderer.view);
 var stage = new PIXI.Container();
+var bg = new PIXI.Container();
+stage.addChild(bg);
+var mg1 = new PIXI.Container();
+stage.addChild(mg1);
+var mg2 = new PIXI.Container();
+stage.addChild(mg2);
+var fg = new PIXI.Container();
+stage.addChild(fg);
+var lighting = new PIXI.Container();
+stage.addChild(lighting);
 
-//PIXI.loader.add('background', 'background.png');
-//PIXI.loader.add('background2', 'background2.png');
-//PIXI.loader.add('background3', 'background3.png');
-//PIXI.loader.add('enemybuggy', 'enemybuggy.png');
-//PIXI.loader.add('enemybullet', 'enemybullet.png');
-//PIXI.loader.add('enemycircle', 'enemycircle.png');
-//PIXI.loader.add('enemydummy', 'enemydummy.png');
-//PIXI.loader.add('enemyshooter', 'enemyshooter.png');
-//PIXI.loader.add('enemyspawner', 'enemyspawner.png');
-//PIXI.loader.add('enemyturret', 'enemyturret.png');
-//PIXI.loader.add('explosion', 'explosion.png');
-//PIXI.loader.add('hudscore', 'hudscore.png');
-//PIXI.loader.add('hudshield', 'hudshield.png');
-//PIXI.loader.add('hudspeed', 'hudspeed.png');
-//PIXI.loader.add('lasers', 'lasers.png');
-//PIXI.loader.add('money', 'money.png');
-//PIXI.loader.add('option', 'option.png');
-//PIXI.loader.add('regularbullet', 'regularbullet.png');
-//PIXI.loader.add('regularbullettrail', 'regularbullettrail.png');
-//PIXI.loader.add('shield', 'shield.png');
-//PIXI.loader.add('splash0', 'splash0.png');
-//PIXI.loader.add('splash1', 'splash1.png');
-//PIXI.loader.add('starfield', 'starfield.png');
-//PIXI.loader.add('thrust', 'thrust.png');
-//PIXI.loader.add('tileset1', 'tileset1.png');
-//PIXI.loader.add('tileset2', 'tileset2.png');
-//PIXI.loader.add('tilesetbreakable', 'tilesetbreakable.png');
-//PIXI.loader.add('title', 'title.png');
-//PIXI.loader.add('vipergirl', 'vipergirl.png');
-//PIXI.loader.add('vipertitlescreenughpleasework', 'vipertitlescreenughpleasework.png');
-PIXI.loader.add('bunny', 'bunny.png');
+var layers = [bg, mg1, mg2, fg, lighting];
 
-var bunnies = [];
+PIXI.loader.add('background', 'bg.png');
+PIXI.loader.add('lighting', 'lighting.png');
+// PIXI.loader.add('dirt', 'dirt.png');
+PIXI.loader.add('growing', 'sprout.png');
+//PIXI.loader.add('flourishing', 'philo_REPLACE.png');
+//PIXI.loader.add('overgrown', 'overgrown.png');
+PIXI.loader.add('watering_can', 'watering-icon.png');
+// PIXI.loader.add('watering_can_tilted', 'watering_can_tilted.png');
+// PIXI.loader.add('birb1', 'birb1_REPLACE.png');
+// PIXI.loader.add('birb2', 'birb2_REPLACE.png');
+PIXI.loader.add('shovel', 'dig-icon.png');
+PIXI.loader.add('bag_of_seeds', 'seed-icon.png');
+PIXI.loader.add('philodendron', 'philo.png');
+PIXI.loader.add('orchid', 'orchi.png');
+PIXI.loader.add('pitcher', 'pitcher.png');
+PIXI.loader.add('heliconia', 'heliconia.png');
+PIXI.loader.add('birb1_flying', 'birbs_f.png');
+PIXI.loader.add('birb1_sitting', 'birbs_s.png');
+PIXI.loader.add('birb2_flying', 'birbs3_f.png');
+PIXI.loader.add('birb2_sitting', 'birbs3_s.png');
+PIXI.loader.add('birb3_flying', 'birbs2_f.png');
+PIXI.loader.add('birb3_sitting', 'birbs2_s.png');
+PIXI.loader.add('seed', 'knob.png');
+PIXI.loader.add('sound_on', 'sound_ON-icon.png');
+PIXI.loader.add('sound_off', 'sound_OFF-icon.png');
 
-function splashtext(text) {
-  return new PIXI.Text(text, 
-                       {font: "50px Arial", fill: "white", align: "center"}
-                      );
-}
+var sprites = [];
+var textures = [];
+var sound_on_icon;
 
-var stages = [
-  splashtext("Hello, it's me"),
-  splashtext("I was wondering if after all these years you'd like to meet"),
-  splashtext("To go over everything"),
-  splashtext("They say that time's supposed to heal ya"),
-  splashtext("But I ain't done much healing"),
-  splashtext("Hello, can you hear me"),
-  splashtext("I'm in California dreaming about who we used to be"),
-  splashtext("When we were younger and free"),
-  splashtext("I've forgotten how it felt before the world fell at our feet"),
+var debug_message = new PIXI.Text("", {font: "50px Arial", fill: "white"});
+var counts = [
+  new PIXI.Text("\u221E", {font: "20px Arial", fill: "black"}),
+  new PIXI.Text("0", {font: "20px Arial", fill: "black"}),
+  new PIXI.Text("0", {font: "20px Arial", fill: "black"}),
+  new PIXI.Text("0", {font: "20px Arial", fill: "black"}),
 ];
 
 function tick() {
@@ -72,13 +60,122 @@ function tick() {
     requestAnimationFrame(tick);
 }
 
-PIXI.loader.once('complete', function () {
-    Module.ccall('begin', null, [], []);
+function reallystart() {
+    // textures[1] = PIXI.loader.resources.dirt.texture;
+    textures[2] = PIXI.loader.resources.growing.texture;
+    // textures[3] = PIXI.loader.resources.flourishing.texture;
+    // textures[4] = PIXI.loader.resources.overgrown.texture;
+    textures[5] = PIXI.loader.resources.watering_can.texture;
+    // textures[6] = PIXI.loader.resources.watering_can_tilted.texture;
+    // textures[7] = PIXI.loader.resources.birb2.texture;
+    // textures[8] = PIXI.loader.resources.birb1.texture;
+    textures[9] = PIXI.loader.resources.philodendron.texture;
+    textures[10] = PIXI.loader.resources.orchid.texture;
+    textures[11] = PIXI.loader.resources.heliconia.texture;
+    textures[12] = PIXI.loader.resources.pitcher.texture;
+    textures[13] = PIXI.loader.resources.shovel.texture;
+    textures[14] = PIXI.loader.resources.bag_of_seeds.texture;
+    textures[15] = PIXI.loader.resources.birb1_flying.texture;
+    textures[16] = PIXI.loader.resources.birb1_sitting.texture;
+    textures[17] = PIXI.loader.resources.birb2_flying.texture;
+    textures[18] = PIXI.loader.resources.birb2_sitting.texture;
+    textures[19] = PIXI.loader.resources.birb3_flying.texture;
+    textures[20] = PIXI.loader.resources.birb3_sitting.texture;
+    textures[21] = PIXI.loader.resources.seed.texture;
+
+    bg.addChild(new PIXI.Sprite(PIXI.loader.resources.background.texture));
+    var x_cursor = 215;
+    var shovel_icon = new PIXI.Sprite(PIXI.loader.resources.shovel.texture);
+    shovel_icon.position.x = x_cursor;
+    x_cursor += 135;
+    shovel_icon.position.y = 850;
+    shovel_icon.anchor.x = 0.5;
+    shovel_icon.anchor.y = 0.5;
+    bg.addChild(shovel_icon);
+    var watering_can_icon = new PIXI.Sprite(PIXI.loader.resources.watering_can.texture);
+    watering_can_icon.position.x = x_cursor;
+    x_cursor += 135;
+    watering_can_icon.position.y = 850;
+    watering_can_icon.anchor.x = 0.5;
+    watering_can_icon.anchor.y = 0.5;
+    bg.addChild(watering_can_icon);
+    for (var i = 0; i < 4; i++) {
+        var bag_icon = new PIXI.Sprite(PIXI.loader.resources.bag_of_seeds.texture);
+        bag_icon.position.x = x_cursor;
+        bag_icon.position.y = 850;
+        bag_icon.anchor.x = 0.5;
+        bag_icon.anchor.y = 0.5;
+        bg.addChild(bag_icon);
+        counts[i].position.x = x_cursor + 30;
+        counts[i].position.y = 850 + 10;
+        bg.addChild(counts[i]);
+        x_cursor += 135;
+    }
+    var sound_off_icon = new PIXI.Sprite(PIXI.loader.resources.sound_off.texture);
+    sound_off_icon.position.x = x_cursor;
+    x_cursor += 135;
+    sound_off_icon.position.y = 850;
+    sound_off_icon.anchor.x = 0.5;
+    sound_off_icon.anchor.y = 0.5;
+    bg.addChild(sound_off_icon);
+
+    sound_on_icon = new PIXI.Sprite(PIXI.loader.resources.sound_on.texture);
+    sound_on_icon.position.x = sound_off_icon.position.x;
+    sound_on_icon.position.y = sound_off_icon.position.y;
+    sound_on_icon.anchor.x = 0.5;
+    sound_on_icon.anchor.y = 0.5;
+    bg.addChild(sound_on_icon);
+
+    lighting.addChild(new PIXI.Sprite(PIXI.loader.resources.lighting.texture));
+    lighting.addChild(debug_message);
     tick();
+
+    renderer.view.onmousedown = function canvas_onmousedown(event) {
+        var x = event.pageX - renderer.view.offsetLeft;
+        var y = event.pageY - renderer.view.offsetTop;
+        Module.ccall('mouse_down', null, ['number', 'number'], [x, y]);
+    }
+    
+    renderer.view.onmouseup = function canvas_onmouseup(event) {
+        var x = event.pageX - renderer.view.offsetLeft;
+        var y = event.pageY - renderer.view.offsetTop;
+        Module.ccall('mouse_up', null, ['number', 'number'], [x, y]);
+    }
+    
+    renderer.view.onmouseover = function canvas_onmouseover(event) {
+        var x = event.pageX - renderer.view.offsetLeft;
+        var y = event.pageY - renderer.view.offsetTop;
+        Module.ccall('mouse_over', null, ['number', 'number'], [x, y]);
+    }
+    
+    renderer.view.onmousemove = function canvas_onmousemove(event) {
+        var x = event.pageX - renderer.view.offsetLeft;
+        var y = event.pageY - renderer.view.offsetTop;
+        Module.ccall('mouse_move', null, ['number', 'number'], [x, y]);
+    }
+    
+    renderer.view.onmouseout = function canvas_onmousemove(event) {
+        Module.ccall('mouse_out', null, [], []);
+    }
+    
+    renderer.view.oncontextmenu = function canvas_oncontextmenu(event) {
+        return false;
+    }
+    Module.ccall('begin', null, [], []);
+    document.querySelector('img').style.display = 'none';
+    document.body.appendChild(renderer.view);
+}
+
+PIXI.loader.once('complete', function () {
+    console.log('pixi loader complete');
+
+    document.querySelector('img').onmousedown = function canvas_onmousedown(event) {
+        reallystart();
+    }
 });
 
 function start() {
-  PIXI.loader.load();
+    PIXI.loader.load();
 }
 
 var Module = {
